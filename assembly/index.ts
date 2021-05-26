287474cff2e8c7e99f2ccdae717c8f767e8eb442
Martin Gostisha <martingostisha@gmail.com>
2021-05-26 13:57:41 -0500

    add weirdSquare

--- assembly/index.ts
+++ assembly/index.ts
@@ -2,3 +2,9 @@
 export function minusOne(n: i32): i32 {
   return n - 1;
 }
+
+export function weirdSquare (n: i32): i32 {
+  if (n === 2) return n * 2;
+  else if (n === 3) return n * 3;
+  else return n * n;
+}
